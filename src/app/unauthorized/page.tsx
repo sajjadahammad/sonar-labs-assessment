1552ea68804446359a1bd620f34c352f3f508aeb
sajjadahammed <sajjad08@protonmail.com>
2025-06-29 22:49:39 +0530

    Fix typo in UnauthorizedPage description for improved clarity

--- src/app/unauthorized/page.tsx
+++ src/app/unauthorized/page.tsx
@@ -17,7 +17,7 @@ export default function UnauthorizedPage() {
           </div>
           <CardTitle className="text-2xl">Access Denied</CardTitle>
           <CardDescription>
-            You don't have permission to access this page
+            You don&apos;t have permission to access this page
           </CardDescription>
         </CardHeader>
         <CardContent className="space-y-4">
